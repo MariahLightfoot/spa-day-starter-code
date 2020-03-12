7a343d295292acb91bb960723ce0e8fb30c83f35
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-03-11 20:01:16 -0500

    added validation for passwords

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +18,16 @@ public class UserController {
         return "user/add";
     }
 
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
 
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+
+        if(!verify.equals(user.getPassword())){
+            return "user/add";
+        }
+
+        return "user/index";
+    }
 }
