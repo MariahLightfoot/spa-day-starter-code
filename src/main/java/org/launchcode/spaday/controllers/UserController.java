85d98bc646d91874de7da7b0463db2f10f7edb5b
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-03-11 19:21:49 -0500

    created basic form for user to enter data and display on /user/add

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,10 +1,21 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm(){
+
+        return "user/add";
+    }
+
 
 }
